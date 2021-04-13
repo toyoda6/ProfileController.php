5723f5abd90ff893dca11cdacefcf5f6c5c9d447
toyodasatoko <likes.glee2.spring@gmail.com>
2021-04-13 03:13:01 +0000

    text10の課題

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+//課題３
+//Route::get('xxx', 'AAAController@bbb');
+
+//課題４
